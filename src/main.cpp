ca794164b9d56d6b7c1057a6ac7eced1ff5881f6
J.W. Jagersma <jwjagersma@gmail.com>
2018-05-10 22:35:16 +0200

    dummy program

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+/* * * * * * * * * * * * * *  joyplot  * * * * * * * * * * * * * */
+/* Copyright (C) 2018 J.W. Jagersma, see COPYING.txt for details */
+
+#include <deque>
+#include <string_view>
+
+int jwdpmi_main(std::deque<std::string_view>)
+{
+    return 0;
+}
